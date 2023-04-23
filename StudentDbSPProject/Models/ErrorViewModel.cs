b78c5d04070efce940d0f219148d5f5517fb8561
Ramrajsaini <ramrajsaini561@gmail.com>
2023-04-23 14:24:03 +0530

    Create a New project In StudentDBProject

---
+++ StudentDbSPProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentDbSPProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
